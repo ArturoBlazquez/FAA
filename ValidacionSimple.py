bea742f5b2b6b1ddd56cef2426ce34d6f73dfb10
nicktrejo <nicktrejo@gmail.com>
2018-10-10 14:53:01 -0300

    Avances en Validacion Simple

--- ValidacionSimple.py
+++ ValidacionSimple.py
@@ -1,8 +1,29 @@
 #Para que haga Nico :)
 
 import EstrategiaParticionado
+import numpy as np
+from Particion import Particion
 
 
 class ValidacionSimple(EstrategiaParticionado):
-    def creaParticiones(self):
+
+
+    def __init__(self, porcentajeTrain):
+        self.porcentajeTrain=porcentajeTrain
+        self.numeroParticiones=1
+        self.nombreEstrategia="Validación Simple"
+
+    def creaParticiones(self, data):
+        indices =np.arange(len(data))
+        desordenados = np.random.shuffle(indices)
+
+        indexDivision= len(data)*self.porcentajeTrain//100
+        train = desordenados[:indexDivision]
+        test = desordenados[indexDivision:]
+        Particion(train,test)
+
+
+
+        np.random.choice(len(data),self.porcentajeTrain,replace=False)
         return
+
