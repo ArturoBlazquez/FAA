8e00ef5d79fccb17b9ab9bc757a592823ebc09e8
nicktrejo <nicktrejo@gmail.com>
2018-11-21 17:05:44 +0100

    KNN terminado

--- P2/ClasificadorKNN_sklearn.py
+++ P2/ClasificadorKNN_sklearn.py
@@ -1,52 +1,48 @@
 from Datos import Datos
-from ValidacionSimple import ValidacionSimple
 from sklearn.neighbors import KNeighborsClassifier
-from ValidacionBootstrap import ValidacionBootstrap
-from ValidacionCruzada import ValidacionCruzada
+from sklearn import preprocessing
 from ValidacionSimple import ValidacionSimple
 import numpy as np
+import itertools
 
-# del ejemplo sacado de internet
-# X = [[0], [1], [2], [3]]   # Vectores de datos (datos train)
-# y = [0, 0, 1, 1]           # Clases de los vectores (de datos train)
+conjuntos_de_datos = ['example1', 'example2', 'example3', 'example4', 'wdbc']
+val = ValidacionSimple(70)
+valores_de_k = [1, 3, 5, 11, 21, 51]
+pesos = ['uniform', 'distance']
 
-example1 = Datos('ConjuntosDatos/example1.data')
+for peso in pesos:
+    print ("\nPeso =", peso)
+    for nombre_fichero in conjuntos_de_datos:
+        datos = Datos('ConjuntosDatos/' + nombre_fichero + '.data')
+        val.creaParticiones(datos.datos) #Se usa Validacion Simple
 
-validaciones = [ValidacionSimple(70), ValidacionCruzada(6), ValidacionBootstrap()]
+        # Normalizo los datos
+        datos_normalizados = preprocessing.normalize(datos.datos, axis=0)
+        datos_normalizados[:,-1] = datos.datos[:,-1]
 
-validaciones[0].creaParticiones(example1.datos)
+        # ndarray para los datos de test que no incluye la clase
+        X = np.zeros((len(val.particiones[0].indicesTrain),len(datos.nominalAtributos)-1))
+        y = []
+        # Completo ndarrays X e y
+        for n, indiceTrain in enumerate(val.particiones[0].indicesTrain):
+            X[n]=datos_normalizados[indiceTrain,:-1]
+            y.append(datos_normalizados[indiceTrain,-1])
 
-# TODO: normalizar los datos (usando funciones de sklearn o nuestra?)
+        print(nombre_fichero, ":", sep='')
+        for k in valores_de_k:
+            SK_KNN = KNeighborsClassifier(n_neighbors=k, weights=peso)    # Clasificador KNN de sklearn
+            SK_KNN.fit(X, y)    # Ingreso los Datos al modelo
 
-X = np.zeros((len(validaciones[0].particiones[0].indicesTest),len(example1.nominalAtributos)-1))
-# ndarray para los datos de test que no incluye la clase
-y = np.zeros((len(validaciones[0].particiones[0].indicesTest),1)) # ndarray para las clases
+            # clasifica todos los datos de datos.datos y los guarda en predicho
+            predicho=[]
+            for dato in datos_normalizados:
+                predicho.append(SK_KNN.predict([dato[:-1]]))
 
-# Completo ndarrays X e y
-for n, indiceTest in enumerate(validaciones[0].particiones[0].indicesTest):
-    X[n]=example1.datos[indiceTest,:-1]
-    y[n] =example1.datos[indiceTest,-1]
+            # errores para los datos de Test y tasa de error correspondiente
+            errores = []
+            for n, indiceTest in enumerate(val.particiones[0].indicesTest):
+                errores.append(datos_normalizados[indiceTest,-1] != predicho[indiceTest])
+            errores = list(itertools.chain.from_iterable(errores))
+            tasa_de_error = sum(errores) / len(errores)
 
-k = 3    # Probar con diferentes valores de k
-peso = 'uniform'  # Probar tambien con 'distance'
-
-# Clasificador KNN de sklearn
-SK_KNN = KNeighborsClassifier(n_neighbors=k, weights=peso)
-SK_KNN.fit(X, y)
-
-# clasifica todos los datos de example.datos y los guarda en predicho
-predicho=[]
-for dat in example1.datos:
-    predicho.append(SK_KNN.predict([dat[:-1]]))
-
-# TODO: error del modelo (tomarlo de Arturo). Comparar 'predicho' con 'y'
-
-verdadero=0
-falso=0
-for i in range(len(y)):
-    if y[i] == predicho[i]:
-        verdadero +=1
-    else:
-        falso +=1
-
-print (verdadero , "," , falso)
+            print("\tTasa de error: k=", k, ":\t\t", tasa_de_error, sep='')
