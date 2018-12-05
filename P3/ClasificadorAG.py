9037422b926c4a85cf93c6d494267ddc77851f4a
Arturo <arturo.blazquezperez@gmail.com>
2018-12-05 18:31:08 +0100

    Genera población inicial acabado

--- P3/ClasificadorAG.py
+++ P3/ClasificadorAG.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, getrandbits
 
 import numpy as np
 
@@ -6,18 +6,24 @@ from Clasificador import Clasificador
 
 
 class ClasificadorAG(Clasificador):
+    REPRESENTACION_ENTERA = 0
+    REPRESENTACION_BINARIA = 1
+    
     def __init__(self):
         super().__init__()
     
-    def entrenamiento(self, datosTrain, atributosDiscretos, diccionario, tipo_de_representacion=0,
+    def entrenamiento(self, datosTrain, atributosDiscretos, diccionario, representacion=REPRESENTACION_ENTERA,
                       numero_de_individuos=100, numero_de_generaciones=100, min_reglas=1, max_reglas=100,
-                      probabilidad_de_mutacion=1, k=5):
+                      probabilidad_de_mutacion=1):
         self.__init__()
         
-        individuos = genera_poblacion_inicial(100, min_reglas, max_reglas, len(atributosDiscretos), k)
+        k = 5
         
-        for i in range(100):
-            fitness = calcula_fitness(individuos, tipo_de_representacion, datosTrain)
+        individuos = genera_poblacion_inicial(numero_de_individuos, min_reglas, max_reglas, len(atributosDiscretos), k,
+                                              representacion)
+        
+        for i in range(numero_de_generaciones):
+            fitness = calcula_fitness(individuos, representacion, datosTrain)
             
             hijos = recombinacion(individuos, fitness)
             
@@ -31,7 +37,7 @@ class ClasificadorAG(Clasificador):
         return np.array(clasificacion)
 
 
-def genera_poblacion_inicial(numero_de_individuos, min_reglas, max_reglas, num_atributos, k):
+def genera_poblacion_inicial(numero_de_individuos, min_reglas, max_reglas, num_atributos, k, representacion):
     individuos = []
     
     for i in range(numero_de_individuos):
@@ -39,7 +45,11 @@ def genera_poblacion_inicial(numero_de_individuos, min_reglas, max_reglas, num_a
         num_reglas = randint(min_reglas, max_reglas)
         
         for j in range(num_reglas):
-            regla = [randint(0, k) for _ in range(num_atributos - 1)] + [randint(0, 1)]
+            if representacion == ClasificadorAG.REPRESENTACION_ENTERA:
+                regla = [randint(0, k) for _ in range(num_atributos - 1)] + [randint(0, 1)]
+            elif representacion == ClasificadorAG.REPRESENTACION_BINARIA:
+                regla = [getrandbits(k) for _ in range(num_atributos - 1)] + [randint(0, 1)]
+            
             individuo.append(regla)
         
         individuos.append(individuo)
@@ -52,7 +62,32 @@ def calcula_fitness(individuos, tipo_de_representacion, datosTrain):
 
 
 def recombinacion(individuos, fitness):
-    return [1, 2, 3]
+    hijos = []
+    
+    for i in range(len(individuos) / 2):
+        padre1 = selecciona_padre(individuos, fitness)
+        padre2 = selecciona_padre(individuos, fitness)
+        
+        hijo1, hijo2 = cruce(padre1, padre2)
+        
+        hijos.append(hijo1)
+        hijos.append(hijo2)
+    
+    return hijos
+
+
+def selecciona_padre(individuos, fitness):
+    #TODO
+
+
+#TODO: ver tipos de representacion
+def cruce(ind1, ind2):
+    '''Cruce de dos individuos para devolver los hijos'''
+    # Numero aleatorio para el cruce entre 1 y el tamanio del menor individuo
+    cruce_index = randint(1, min(len(ind1), len(ind2)))
+    hijo1 = ind1[:cruce_index] + ind2[cruce_index:]
+    hijo2 = ind2[:cruce_index] + ind1[cruce_index:]
+    return hijo1, hijo2
 
 
 def mutacion(hijos, probabilidad_de_mutacion):
